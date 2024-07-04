591dafce0fdb1053ded4ceeb98df7b0bfd2b1372
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2024-07-04 14:23:54 -0300

    Melhora consulta de lançamento

--- gerenciador-ativos-frontend/src/app/components/lancamento/lancamento.component.ts
+++ gerenciador-ativos-frontend/src/app/components/lancamento/lancamento.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Lancamento } from '../../common/lancamento';
 import { LancamentoService } from '../../services/lancamento.service';
+import { ContaCorrenteService } from '../../services/conta-corrente.service';
 
 @Component({
   selector: 'app-lancamento',
@@ -11,10 +12,28 @@ export class LancamentoComponent {
 
   public lancamento: Lancamento = new Lancamento(1, "ENTRADA", 0, "", new Date().toISOString());
 
-  constructor(private lancamentoService: LancamentoService) {}
+  public contaCorrenteSaldo: number = 0;
+
+  constructor(private lancamentoService: LancamentoService, private contaCorrenteService: ContaCorrenteService) {}
+
+  ngOnInit(): void {
+    this.consultarSaldo();
+  }
+
+  consultarSaldo(): void {
+    this.contaCorrenteService.consultarSaldo(new Date().toISOString()).subscribe({
+      next: response => { this.contaCorrenteSaldo = response; console.log(response) },
+      error: response => console.error(response),
+      complete: () => console.log(this.contaCorrenteSaldo)
+    });
+  }
 
   incluirLancamento(): void {
-    
+    this.lancamentoService.incluirLancamento(this.lancamento).subscribe({
+      next: response => { this.lancamento = response; },
+      error: response => console.error(response),
+      complete: () => this.consultarSaldo()
+    });
   }
 
 }
--- gerenciador-ativos-frontend/src/app/services/lancamento.service.ts
+++ gerenciador-ativos-frontend/src/app/services/lancamento.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Lancamento } from '../common/lancamento';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LancamentoService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  incluirLancamento(lancamento: Lancamento): Observable<any> {
+    return this.httpClient
+      .post<Lancamento>("http://localhost:8080/api/v0/lancamentos", lancamento);
+  }
 }
