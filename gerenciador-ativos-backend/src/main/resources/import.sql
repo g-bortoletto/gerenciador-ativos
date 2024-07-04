99023f04ff96a53324a50fb43d539fec7b888f10
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2024-07-04 14:05:01 -0300

    Corrige bd movimentações para aplicação teste.

--- gerenciador-ativos-backend/src/main/resources/import.sql
+++ gerenciador-ativos-backend/src/main/resources/import.sql
@@ -21,8 +21,8 @@ INSERT INTO valor_mercado (id, ativo_financeiro_id, valor, data) VALUES (10, 5, 
 
 ALTER TABLE valor_mercado ALTER COLUMN id RESTART WITH 11;
 
-INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VALUES (0, 1000.0, 1, '2024-07-01T01:00:00',  1, '');
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (100.0, 0, 1000.0, 1, 1, '2024-07-01T01:00:00', 1);
+INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VALUES (0, 1100.0, 1, '2024-07-01T01:00:00',  1, '');
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (100.0, 1, 1000.0, 1, 1, '2024-07-01T01:00:00', 1);
 
 INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VALUES (0, 100.0, 1, '2024-07-01T01:00:00',  2, '');
 INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VALUES (0, 100.0, 1, '2024-07-02T01:00:00',  3, '');
@@ -35,16 +35,16 @@ INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VAL
 INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VALUES (1,  50.0, 1, '2024-07-04T03:00:00', 10, '');
 INSERT INTO lancamento (tipo, valor, conta_corrente_id, data, id, descricao) VALUES (1, 100.0, 1, '2024-07-01T06:00:00', 11, '');
 
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0, 100.0, 1, 1, '2024-07-01T01:00:00',  2);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0, 100.0, 2, 1, '2024-07-02T01:00:00',  3);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0, 100.0, 3, 1, '2024-07-03T01:00:00',  4);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0, 100.0, 4, 1, '2024-07-04T01:00:00',  5);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0, 100.0, 5, 1, '2024-07-01T05:00:00',  6);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1,  50.0, 1, 1, '2024-07-01T03:00:00',  7);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1,  50.0, 2, 1, '2024-07-02T03:00:00',  8);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1,  50.0, 3, 1, '2024-07-03T03:00:00',  9);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1,  50.0, 4, 1, '2024-07-04T03:00:00', 10);
-INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1, 100.0, 5, 1, '2024-07-01T06:00:00', 11);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1, 100.0, 1, 1, '2024-07-01T01:00:00',  2);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1, 100.0, 2, 1, '2024-07-02T01:00:00',  3);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1, 100.0, 3, 1, '2024-07-03T01:00:00',  4);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1, 100.0, 4, 1, '2024-07-04T01:00:00',  5);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 1, 100.0, 5, 1, '2024-07-01T05:00:00',  6);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0,  50.0, 1, 1, '2024-07-01T03:00:00',  7);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0,  50.0, 2, 1, '2024-07-02T03:00:00',  8);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0,  50.0, 3, 1, '2024-07-03T03:00:00',  9);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0,  50.0, 4, 1, '2024-07-04T03:00:00', 10);
+INSERT INTO movimentacao (quantidade, tipo, valor, ativo_financeiro_id, conta_corrente_id, data, id) VALUES (10.0, 0, 100.0, 5, 1, '2024-07-01T06:00:00', 11);
 
 ALTER TABLE lancamento ALTER COLUMN id RESTART WITH 12;
 ALTER TABLE movimentacao ALTER COLUMN id RESTART WITH 12;
